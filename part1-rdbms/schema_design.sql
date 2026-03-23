f414daa0762a2bf03a73419d630c35ce3fad4e41
ravalimaddi99-code <ravalimaddi99@gmail.com>
2026-03-23 21:53:59 +0530

    Initialize database and create customers table
    Added SQL commands to set the database and create the customers table.

--- part1-rdbms/schema_design.sql
+++ part1-rdbms/schema_design.sql
@@ -1,1 +1,19 @@
+USE assignment_db;
+select database();
+select * from orders_flat;
+-- Use database
+USE assignment_db;
 
+-- Drop tables if already exist (to avoid errors)
+DROP TABLE IF EXISTS order_items;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS sales_reps;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS customers;
+
+-- Customers Table
+CREATE TABLE customers (
+    customer_id VARCHAR(10) PRIMARY KEY,
+    customer_name VARCHAR(100) NOT NULL,
+    city VARCHAR(50) NOT NULL
+);
